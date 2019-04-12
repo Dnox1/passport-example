56c285d557623d3c7a2f6c4b3b9d4a59f7cc0187
Dnox <david@nestrategia.com>
2019-04-12 21:30:26 +0200

    lesson done

---
+++ routes/auth-routes.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const authRoutes = express.Router();
+
+const User = require("../models/user");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+
+authRoutes.get("/signup", (req, res, next)=> {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {message: "Indicate username and password"});
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("auth/signup", {message:"The username already exist"});
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User ({
+        username,
+        password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    })
+  })
+  .catch(error => { next(error)
+  })
+});
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+authRoutes.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+authRoutes.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+authRoutes.get("/logout", (req, res) =>{
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = authRoutes; 
